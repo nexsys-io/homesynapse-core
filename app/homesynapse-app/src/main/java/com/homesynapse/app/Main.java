6c6dd3381c4ff6d86e77f8614e3dc532253dd52c
Nick Smith <nickdsmith1@gmail.com>
2026-06-07 18:06:20 -0500

    Fixed typo in print statement... Lol

--- app/homesynapse-app/src/main/java/com/homesynapse/app/Main.java
+++ app/homesynapse-app/src/main/java/com/homesynapse/app/Main.java
@@ -17,6 +17,6 @@ public final class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("HomeSynapse Core — not yet implemented");
+        System.out.println("HomeSynapse Core not yet implemented");
     }
 }
